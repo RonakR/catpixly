e6e3488f5d211fb72921cbed0b08ff5bfbc4cb51
Ronak Raithatha <ronaksraithatha@gmail.com>
2021-06-02 00:33:23 -0400

    feat: added small low code frontend

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import 'dotenv/config'
 import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
 import imageRoutes from './routes/image.js'
 import { createDbConnection } from './dbConnection.js'
@@ -15,7 +17,9 @@ app.use(express.json())
 app.use('/images', imageRoutes)
 
 app.get('/', (req, res) => {
-	res.send('Hello World')
+	// ES6 modules do not have __dirname or __filename defined
+	const __dirname = path.dirname(fileURLToPath(import.meta.url))
+	res.sendFile(path.resolve(__dirname + '/index.html'))
 })
 
 app.use((req, res) => {
